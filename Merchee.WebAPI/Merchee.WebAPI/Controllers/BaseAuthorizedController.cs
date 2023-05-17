2b103bfba873c7fc5f3bceb32a9c2a24db32549f
Viktor Zherebnyi <craftingman111@gmail.com>
2023-05-17 17:04:31 +0300

    Added base controllers

---
+++ Merchee.WebAPI/Merchee.WebAPI/Controllers/BaseAuthorizedController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Merchee.WebAPI.Controllers
+{
+    [Authorize]
+    public class BaseAuthorizedController : Controller
+    {
+        protected Guid UserId
+        {
+            get
+            {
+                return Guid.Parse(this.HttpContext.User.Claims
+                    .First(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            }
+        }
+
+        protected Guid CompanyId
+        {
+            get
+            {
+                return Guid.Parse(this.HttpContext.User.Claims
+                    .First(c => c.Type == "company_id").Value);
+            }
+        }
+    }
+}
---
+++ Merchee.WebAPI/Merchee.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,42 @@
+﻿using FluentResults;
+using Merchee.BLL.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Merchee.WebAPI.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected IActionResult HandleResult<TResult>(Result<TResult> result)
+        {
+            if (result.IsSuccess)
+            {
+                return Ok(result.Value);
+            }
+
+            return HandleFailedResult(result.ToResult());
+        }
+
+        protected IActionResult HandleResult(Result result)
+        {
+            if (result.IsSuccess)
+            {
+                return Ok();
+            }
+
+            return HandleFailedResult(result);
+        }
+
+        protected IActionResult HandleFailedResult(Result result)
+        {
+            var error = result.Errors.FirstOrDefault();
+            if (error is null || string.IsNullOrEmpty(error.Message))
+                return Problem("Unexpected internal server error");
+            if (error is NotFoundError)
+                return NotFound();
+            if (error is BadRequestError)
+                return BadRequest();
+
+            return Problem(error.Message);
+        }
+    }
+}
